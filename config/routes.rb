a5113a8094c56d6bbbb312d00b1384241fd4ec0e
Francesca Dreith <francesca.dreith@gmail.com>
2020-10-31 14:24:43 -0700

    conect with client

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :care_reminders
-  resources :records
-  resources :events
-  resources :pets
-  resources :user_households
-  resources :households
-  resources :users
+  scope '/api' do 
+    resources :care_reminders
+    resources :records
+    resources :events
+    resources :pets
+    resources :user_households
+    resources :households
+    resources :users
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
