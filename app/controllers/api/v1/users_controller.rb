82403176d5ed5591d8fa77bbabcc9521e6dc8dea
Francesca Dreith <francesca.dreith@gmail.com>
2020-11-11 21:11:36 -0800

    serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,13 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render jsonapi: @users, include: [:households, ]],
+    fields: {}
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render jsonapi: @user
   end
 
   # POST /users
@@ -18,7 +19,7 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render jsonapi: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -27,7 +28,7 @@ class Api::V1::UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render jsonapi: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
